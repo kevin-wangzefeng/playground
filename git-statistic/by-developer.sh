99774df639fdf0e7c3603490cf0f0cde51da721c
Kevin Wang <kevinwzf0126@gmail.com>
2023-08-10 11:27:48 +0800

    add filter to ignore .svg changes
    Signed-off-by: Kevin Wang <kevinwzf0126@gmail.com>

--- git-statistic/by-developer.sh
+++ git-statistic/by-developer.sh
@@ -5,7 +5,7 @@
 function contributions {
   git log --no-merges --since ${SINCE_DATE} --author "${AUTHOR}"  --numstat |\
     grep -v "vendor" |\
-    grep -Pv "Date:|insertion|deletion|file|Bin|generated|yaml|html|go\.sum|\.proto" | sort -k3 |\
+    grep -Pv "Date:|insertion|deletion|file|Bin|generated|yaml|html|go\.sum|\.proto|\.svg" | sort -k3 |\
     grep -P "^\d+\t\d+" |\
     awk 'BEGIN{total=0}{total+=$1+$2}END{print total}'
 }
