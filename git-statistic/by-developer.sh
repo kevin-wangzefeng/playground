9a771545c36eac60fa368fb15b50a6e7ef7250a1
Kevin Wang <kevinwzf0126@gmail.com>
2023-12-25 21:11:34 +0800

    add git statistic contributions-period and changes-period function
    Signed-off-by: Kevin Wang <kevinwzf0126@gmail.com>

--- git-statistic/by-developer.sh
+++ git-statistic/by-developer.sh
@@ -9,3 +9,25 @@ function contributions {
     grep -P "^\d+\t\d+" |\
     awk 'BEGIN{total=0}{total+=$1+$2}END{print total}'
 }
+
+# AUTHOR="foo@bar.com"
+# SINCE_DATE="2022-01-01"
+# UNTIL_DATE="2023-01-01"
+function contributions-period {
+  git log --no-merges --since ${SINCE_DATE} --until ${UNTIL_DATE} --author "${AUTHOR}"  --numstat |\
+    grep -v "vendor" |\
+    grep -Pv "Date:|insertion|deletion|file|Bin|generated|yaml|html|go\.sum|\.proto|\.svg" | sort -k3 |\
+    grep -P "^\d+\t\d+" |\
+    awk 'BEGIN{total=0}{total+=$1+$2}END{print total}'
+}
+
+# AUTHOR="foo@bar.com"
+# SINCE_DATE="2022-01-01"
+# UNTIL_DATE="2023-01-01"
+function changes-period {
+  git log --no-merges --since ${SINCE_DATE} --until ${UNTIL_DATE} --author "${AUTHOR}"  --numstat |\
+    grep -v "vendor" |\
+    grep -Pv "Date:|insertion|deletion|file|Bin|generated|yaml|html|go\.sum|\.proto|\.svg" |\ # | sort -k3 |\
+    grep -P "^\d+\t\d+|^commit|^Author"
+    #grep -P "^\d+\t\d+"
+}
